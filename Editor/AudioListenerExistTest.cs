b23f2172e3509a630a40383cf00feab463197e8c
baba_s <shotababa1206@gmail.com>
2020-09-25 21:13:18 +0900

    first commit

---
+++ Editor/AudioListenerExistTest.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+
+namespace Kogane.Internal
+{
+	internal sealed class AudioListenerExistTest
+	{
+		[Category( nameof( Kogane ) )]
+		[Test]
+		public void Audio_Listener_が存在しない()
+		{
+			AllGameObjectTester.Test( gameObject => gameObject.GetComponent<AudioListener>() == null );
+		}
+	}
+}
